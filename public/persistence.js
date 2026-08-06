5a0fcb84ddb88f4b7d314ac2b4b4ddda7bd78517
jlibranda <jlibranda@gmail.com>
2026-08-07 00:37:09 +0800

    Fix IIFE closure in persistence.js

--- public/persistence.js
+++ public/persistence.js
@@ -1,4 +1,5 @@
-$ cat /home/user/Project2/public/persistence.js
+(function () {
+  'use strict';
 
   var remoteHosts = /(?:vercel\.app|chatgpt\.site)$/i;
   var apiHost = remoteHosts.test(location.hostname) ? 'https://sproutripple-ph-production.up.railway.app' : location.origin;
